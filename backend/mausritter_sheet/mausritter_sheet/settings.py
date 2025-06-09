43d7a9649562a38569f68dad8eb03d51bfd8cff9
GabiiSmith <gabiismith2004@gmail.com>
2025-06-09 18:40:19 -0300

    Fix: Erro de cors resolvido

--- backend/mausritter_sheet/mausritter_sheet/settings.py
+++ backend/mausritter_sheet/mausritter_sheet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mausritter_sheet.urls'
@@ -111,6 +113,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
