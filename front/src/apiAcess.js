889bf232e857c5ebbe419bd5a7511405566edbea
Danilus04 <danilogarciaba@hotmail.com>
2025-06-16 17:56:28 -0300

    Fix: fixed weird baseURL

--- front/src/apiAcess.js
+++ front/src/apiAcess.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const port = 8000
 
 const api = axios.create({
-    baseURL: `http://localhost:${port}/`
+    baseURL: `http://localhost:${port}/app/`
 });
 
 export default api;
