27d8aa9084c444678eb514af558b36d5f68eb22a
Danilus04 <danilogarciaba@hotmail.com>
2025-06-23 16:03:40 -0300

    feat: modelo de usuario adicionado no admin

--- backend/mausritter_sheet/app/admin.py
+++ backend/mausritter_sheet/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Item, UserItem
+from .models import Item, UserItem, User
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(UserItem)
+admin.site.register(User)
