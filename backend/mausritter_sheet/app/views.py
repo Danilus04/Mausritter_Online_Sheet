11a959651baccae9157a2cce480fdc11d547ba13
Pedro Aquiles Vieira Soares <54330664+pedroaquiles@users.noreply.github.com>
2025-06-27 18:25:38 +0000

    Como jogador, Quero não precisar autenticar para criar uma ficha temporária, Para que eu possa  criar  ficahas sem precisar autenticar. (3) Fixes #7

--- backend/mausritter_sheet/app/views.py
+++ backend/mausritter_sheet/app/views.py
@@ -2,9 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
-from .serializers import ItemSerializer, UserItemSerializer, UserSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import ItemSerializer, UserItemSerializer, UserSerializer, UserRegisterSerializer
 from django.contrib.auth.hashers import check_password
-from .models import Item, UserItem, User
+from django.contrib.auth import authenticate
+from .models import Item, UserItem
 
 # Endpoint: GET /items/, POST /items/
 class ItemListCreateAPIView(generics.ListCreateAPIView):
@@ -29,19 +32,21 @@ class UserItemListCreateAPIView(generics.ListCreateAPIView):
     queryset = UserItem.objects.all()
     serializer_class = UserItemSerializer
 
-class LoginView(APIView):
+class RegisterUserAPIView(APIView):
+    permission_classes = (AllowAny,)
+
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        try:
-            user = User.objects.get(nameUser=username)
-            # Se as senhas estiverem em texto puro, use:
-            if user.passwordUser == password:
-                return Response({'message': 'Login successful', 'user_id': user.idUser})
-            # Se usar hash, troque por:
-            # if check_password(password, user.passwordUser):
-            #     return Response({'message': 'Login successful', 'user_id': user.idUser})
-            else:
-                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        except User.DoesNotExist:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                    'message': 'Usuário registrado com sucesso.'
+                }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,7 +4,8 @@ import './App.css'
 import Ficha from './App/ficha'  // Importa o componente Ficha
 import ItensDefault from './App/ItensDefault'  // Importa o componente ItensDefault
 import Teste from './App/teste'
-import Login from './Login'
+import Login from './components/Login'
+import Register from './components/Register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path="/teste" element={<Teste />} />
         </Routes>
       </Router>
+      <Register />
     </>
   )
 }
