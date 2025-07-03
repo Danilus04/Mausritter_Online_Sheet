bec550b10849ca88d26c8c2669f6eba680a93981
unknown <gabiismith2004@gmail.com>
2025-07-03 00:33:29 -0300

    Fix: css arrumado para criar itens

--- front/src/components/ui/CheckBoxInput.jsx
+++ front/src/components/ui/CheckBoxInput.jsx
@@ -1,14 +1,10 @@
 "use client";
-import "../styles/CheckBoxInput.css"; 
+import "../styles/CheckBoxInput.css";
 
 export default function CheckboxInput({ label, checked, onChange }) {
   return (
     <label className="checkbox-label">
-      <input
-        type="checkbox"
-        checked={checked}
-        onChange={e => onChange(e.target.checked)}
-      />
+      <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
       <span className="checkbox-text">{label}</span>
     </label>
   );
